f21f06c2d82836eb05747353358e4b15249ac71d
Saša Mršić <91570389+mrsicsasa@users.noreply.github.com>
2025-02-06 12:17:23 +0100

    Use inheritance and refactor the code

--- book-network/src/main/java/com/mrsicsasa/book/book/Book.java
+++ book-network/src/main/java/com/mrsicsasa/book/book/Book.java
@@ -1,19 +1,20 @@
 package com.mrsicsasa.book.book;
 
+import com.mrsicsasa.book.common.BaseEntity;
 import jakarta.persistence.*;
-import org.springframework.data.annotation.CreatedBy;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
 
-import java.time.LocalDateTime;
+@Getter
+@Setter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
-@EntityListeners(AuditingEntityListener.class)
-public class Book {
-    @Id
-    @GeneratedValue
-    private Integer id;
+public class Book extends BaseEntity {
     private String title;
     private String authorName;
     private String isbn;
@@ -21,16 +22,4 @@ public class Book {
     private String bookCover;
     private boolean archived;
     private boolean shareable;
-    @CreatedDate
-    @Column(nullable = false, updatable = false)
-    private LocalDateTime createdDate;
-    @LastModifiedDate
-    @Column(insertable = false)
-    private LocalDateTime lastModifiedDate;
-    @CreatedBy
-    @Column(nullable = false, updatable = false)
-    private Integer createdBy;
-    @LastModifiedBy
-    @Column(insertable = false)
-    private Integer lastModifiedBy;
 }
